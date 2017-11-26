ca84500aa825c431cac0450a8a1f37faeefe0356
Alexandre A. Bizri <alexandre@bizri.fr>
2017-11-26 16:03:39 +0100

    Ajoute un début de controlleur pour l'inscription de l'utilisateur

---
+++ website/Controllers/user.php
@@ -0,0 +1,36 @@
+<?php
+namespace Controllers;
+
+/**
+ * Class User
+ * @package Controllers
+ */
+class User
+{
+    /**
+     * join subscribes a user
+     */
+    public function join() {
+        // Get the data
+        $nick = $_POST["nick"];
+        $email = $_POST["email"];
+        $password_clear = $_POST["password"];
+
+        // Check if an entity with the same e-mail and/or nickname exists
+        if (\Repositories\Users::findByNick($nick) != null) {
+            echo "A user with this nick already exists";
+        } else if (\Repositories\Users::findByEmail($email) != null) {
+            echo "A user with this email already exists";
+        }
+
+        // Create the entity
+        $u = new \Entities\User($nick,$email,$password_clear);
+
+        // Insert it
+        try {
+            \Repositories\Users::insert($u);
+        } catch (\Exception $e) {
+            echo "Error inserting user".$e;
+        }
+    }
+}
