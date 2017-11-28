b6abb00638ef051eb0a84f920b2bff9547547242
Randy <randy.arnold@hotmail.fr>
2017-11-28 09:49:17 +0100

    Ajoute les propriété
    entité propriété et repertoire propriété

---
+++ website/Entities/property.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arnoldrandy
+ * Date: 27/11/2017
+ * Time: 11:46
+ */
+namespace Entities;
+
+class Property
+{
+    // valeurs de l'objet propriete
+    public $id;
+    public $name;
+    public $address;
+    public $creation_date;
+    public $last_updated;
+}
---
+++ website/Repositories/properties.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arnoldrandy
+ * Date: 27/11/2017
+ * Time: 11:48
+ */
+
+namespace Repositories;
+
+
+class Properties
+{
+
+}
