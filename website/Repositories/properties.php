4cfd98c7f2e6f238ec41abe501ac8fc27fcdbe5e
Dinesh Anthonipillai <dinesh77240@gmail.com>
2017-12-04 11:46:51 +0100

    Developpe Repositories\Properties

--- website/Repositories/properties.php
+++ website/Repositories/properties.php
@@ -9,7 +9,39 @@
 namespace Repositories;
 
 
-class Properties
+class Properties extends Repository
 {
+    /**
+     * insert adds an entity to the database
+     *
+     * @param \Entities\Property $p
+     * @throws \Exception
+     */
+    public static function insert(\Entities\Property $p): void
+    {
+        // SQL
+        $sql = "INSERT INTO properties (name, address)
+          VALUES (:name, :address);";
 
+        // Prepare statement
+        $sth = parent::db()->prepare($sql, parent::$pdo_params);
+
+        // Prepare data to be inserted
+        $data = [
+            ":name" => $p->getName(),
+            ":address" => $p->getAddress(),
+        ];
+
+        // Execute request
+        $sth->execute($data);
+
+        // Get ID of the insert
+        $id = parent::db()->lastInsertId();
+        if ($p->setId($id) == false) {
+            throw new \Exception("error setting id");
+        }
+
+        // We should now pull to populate ID & Times
+        self::pull($p);
+    }
 }
