7eb681aa8c8e13cd7fa649c2dc331bbfe27bdecc
Alexandre A. Bizri <alexandre@bizri.fr>
2017-12-05 13:40:29 +0100

    Construit le routage, qui est maintenant fonctionnel, incorpore les session dans l'index, et modifie les controlleurs pour refleter les changements

--- website/Helpers/autoloader.php
+++ website/Helpers/autoloader.php
@@ -5,3 +5,18 @@
  * Date: 12/5/17
  * Time: 12:16 PM
  */
+/**
+ * Charge une classe en utilisant son Namespace comme structure de dossier
+ *
+ * @param string $classname
+ * @throws Exception
+ */
+function __autoload(string $classname)
+{
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
+    $path = __DIR__.'/../'.$path;
+    if (!file_exists($path)) {
+        throw new Exception("File does not exist : $path");
+    }
+    require_once($path);
+}
