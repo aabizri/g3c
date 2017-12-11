92f6fbcf769e553d72163e15e4e3a1bd9f300c24
Alexandre A. Bizri <alexandre@bizri.fr>
2017-12-11 10:37:45 +0100

    Renomme vues en Views

--- website/Views/Layout/header/header.php
+++ website/Views/Layout/header/header.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<header>
-    This is the header
-</header>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="header.css" />
+    </head>
+
+    <body>
+    <header>
+        <div id="Logo"> <a href="paged'accueil.html"><img src=logopetit.png alt="logopetit.png"> </a></div>
+        <p id="Nom">LiveWell</p>
+        <p id="Phrasedaccroche"><i>Votre sécurité est notre priorité.</i></p>
+        <p id="Nomdelapage">Nom de la page</p>
+    </header>
+
