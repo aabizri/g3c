d74d75a7af09c37ab90c35a95a3de5129f48e860
Bryanau <Bryan.nlg.93@gmail.com>
2017-12-11 10:40:42 +0100

    Réorganise

--- website/vues/Users/connexion/connexion.php
+++ website/vues/Users/connexion/connexion.php
@@ -7,6 +7,8 @@
     </head>
     <link rel="stylesheet" type="text/css" href="connexion.css">
 
+    <body>
+
     <header>
     	<img id="Logo" src="Logofond2.png">
     	<p id="livewell">Livewell</p>
@@ -27,7 +29,6 @@
     	<p id="inscription">Vous n'êtes pas inscrit? Cliquez <a href="D:\Bryan\ISEP\APP G3C\Info\Site\Page d'inscription\Inscription.html">ici</a></p>
     </header>
 
-    <body>
 		<div id="explications">
 			<div id="confort">
 				<h3>CONFORT</h3>
@@ -53,12 +54,3 @@
 				un réel confort en toute sécurité.<br>La technologie du futur dans votre habitat d’aujourd’hui.
                 <br>Tout cela est possible grâce à <b>Livewell</b>.</p></i>
 		</div>
-	</body>
-    <footer>
-    	<p id="SAV">Contacter le service client <a href="SAV">ici</a><br><a href="FAQ">FAQ</a><br><a href="Mentions légales">Mentions légales</a></p>
-    	<p class="Renseignements" id="socialmedias">Réseaux sociaux</p>
-              <a href="https://www.facebook.com/"><img id="Fb" src="Fb.png"></a>
-              <a href="https://www.twitter.com/"><img id="Twitter" src="Twitter.png"></a>
-              <p id="rights">2017 Livewell&copy all rights reserved</p>
-    </footer>
-</html>
