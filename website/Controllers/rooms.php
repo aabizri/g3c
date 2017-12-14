a17190a8dda5d024951cb2bc1dac06e8a54509b5
Eytaan <Eytan.Azria@isep.fr>
2017-12-14 11:36:11 +0100

    Créer le controllers ajouter une pièce

---
+++ website/Controllers/rooms.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Controllers;
+
+use Repositories;
+use Entities;
+
+/**
+ * Class Rooms
+ * @package Controllers
+ */
+
+class Rooms
+{
+    /*Ajouter une pièce*/
+    public function postNouvellepiece(array $get, array $post): void
+    {
+        /*Vérifier que les données existent*/
+        $required = ["name"];
+        if (empty($post("name"))) {
+            echo "Il manque : "."name";
+            return;
+            }
+        
+        /*Assigne les valeurs*/
+        $name = $post["name"];
+
+
+        /*Créer l'entité*/
+        $r = new Entities\Room();
+        $r->setName($name);
+
+
+        /*Insérer l'entité dans la bdd*/
+        try {
+            Repositories\Rooms::insert($r);
+        } catch (\Exception $e) {
+            echo "Erreur" . $e;
+        }
+    }
+}
