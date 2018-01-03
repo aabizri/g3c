e21eb2dad4e078176a7351ceef407dcf9f7972a0
Eytaan <Eytan.Azria@isep.fr>
2018-01-03 01:40:13 +0100

    Améliore la page de la boutique

---
+++ website/Views/Boutique/boutique/quantité.js
@@ -0,0 +1,13 @@
+<label for="q">Quantité: </label>
+<select id="qt" name="q">
+    <option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+</select><br>
+    <button type="button" class="add-to-cart" data-id="432" data-name="Pastilles" data-price="29,00" data-weight="97" data-url="/produit/pastilles-anti-douleurs/">Ajouter au panier</button>
