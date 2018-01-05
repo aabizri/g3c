423feaffba1fcb93f9fd42efcdec43be953f7562
Eytaan <Eytan.Azria@isep.fr>
2018-01-06 00:08:17 +0100

    Insère le display manager de master dans dev-store

--- website/Helpers/DisplayManager.php
+++ website/Helpers/DisplayManager.php
@@ -26,6 +26,7 @@ class DisplayManager
         "moncompte" => "Users",
         "mespieces" => "Rooms",
         "mesperipheriques"=> "Peripherals",
+        "mysessions" => "Users",
     ];
 
     private static function subroot(): string {
@@ -113,10 +114,10 @@ class DisplayManager
      * @param array $data
      * @throws \Exception
      */
-    public static function display(string $name, array $data): void {
+    public static function display(string $name, array $data = []): void {
         // Resolve components
         $components = self::resolveMultipleComponents(["head","header",$name,"footer"]);
-        
+
         // For each, extract the css & php
         $php = array();
         $css = array();
@@ -149,5 +150,25 @@ class DisplayManager
         foreach ($php as $toinc) {
             include($toinc);
         }
+    }
+
+    /**
+     * Redirects to destination with 302 (temporary redirect)
+     *
+     * @param string $destination
+     */
+    public static function redirectToController(string $category, string $action): void
+    {
+        self::redirectToPath("index.php?c=$category&a=$action");
+    }
+
+    /**
+     * Redirects to destination with 302 (temporary redirect)
+     *
+     * @param string $destination
+     */
+    public static function redirectToPath(string $destination): void
+    {
+        header("Location: " . \Helpers\DisplayManager::absolutifyURL($destination));
     }
 }
