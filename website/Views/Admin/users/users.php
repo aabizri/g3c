4d8c02413b54ba2a0723149a5afb256bcb134291
Alexandre A. Bizri <alexandre@bizri.fr>
2018-01-05 23:02:32 +0100

    Rend le système fonctionnel sans rechargement de page ou redirection.
    (VICTOIRE)

--- website/Views/Admin/users/users.php
+++ website/Views/Admin/users/users.php
@@ -35,8 +35,12 @@
             // Select
             let tbody = document.getElementById("users");
 
+            // Purge inside
+            tbody.innerHTML = "";
+
             // Insert
             for (let user of users) {
+                // Create the row
                 let row = document.createElement("tr");
 
                 // Properties
@@ -44,10 +48,17 @@
 
                 // Deal with them
                 for (let property of properties) {
+                    // Create the td
                     let td = document.createElement("td");
+
+                    // Set the necessary attributes
                     td.setAttribute("id", "user-" + user.id);
                     td.setAttribute("class", property);
+
+                    // Place the content
                     let content = document.createTextNode(user[property]);
+
+                    // Add it all up
                     td.appendChild(content);
                     row.appendChild(td);
                 }
@@ -57,7 +68,46 @@
             }
         };
 
-        xhr.open("GET", "index.php?c=Admin&a=Users&v=json", true);
-        xhr.send();
+        function getCurrentParameters() {
+            let hash = window.location.hash.substr(1);
+
+            let result = hash.split('&').reduce(function (result, item) {
+                let parts = item.split('=');
+                result[parts[0]] = parts[1];
+                return result;
+            }, {});
+
+            return result;
+        }
+
+        function setCurrentParameters(params) {
+            let hash = "";
+            for (property in params) {
+                hash += "&" + property + "=" + params[property];
+            }
+            hash[0] = "#";
+            window.location.hash = hash;
+        }
+
+        function hydrate() {
+            // Get the parameters
+            let params = getCurrentParameters();
+
+            // Build the new URL
+            let url = new URL(window.location.href);
+            url.searchParams.set("v", "json");
+            for (property in params) {
+                url.searchParams.set(property, params[property]);
+            }
+
+            // Execute request
+            xhr.open("GET", url.href, true);
+
+            // Send
+            xhr.send();
+        }
+
+        window.addEventListener("hashchange", hydrate, false);
+        hydrate();
     </script>
 </main>
