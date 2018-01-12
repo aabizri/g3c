18916ff135639c102e8a8224a1909d728b40c967
Randy <randy.arnold@hotmail.fr>
2018-01-12 23:26:49 +0100

    CSS baisser le formulaire

--- website/Views/Properties/nouvellepropriete/nouvellepropriete.php
+++ website/Views/Properties/nouvellepropriete/nouvellepropriete.php
@@ -1,4 +1,8 @@
-<form action="index.php?c=Property&a=New" method="POST">
+<head>
+    <link rel="stylesheet" href="nouvellepropriete.css" />
+</head>
+
+<form id="form" action="index.php?c=Property&a=New" method="POST">
     <label>Nom : </label><input type="text" name="name"/>
     <label>Adresse : </label><input type="text" name="address"/>
     <input type="submit" value="Valider"/>
