9387762db665d15eb002c85901713aa56265fd6c
Randy <randy.arnold@hotmail.fr>
2018-01-12 13:46:13 +0100

    post

--- website/Views/Properties/nouvellepropriete/nouvellepropriete.php
+++ website/Views/Properties/nouvellepropriete/nouvellepropriete.php
@@ -1,4 +1,4 @@
-<form action="index.php?c=Property&a=New" method="post">
+<form action="index.php?c=Property&a=New" method="POST">
     <label>Nom : </label><input type="text" name="name"/>
     <label>Adresse : </label><input type="text" name="address"/>
     <input type="submit" value="Valider"/>
