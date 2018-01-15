0552ef5635c27f48c026f27466d67a39a9900008
Eytaan <Eytan.Azria@isep.fr>
2018-01-15 11:19:52 +0100

    Rajoute les codes d'erreur si l'ID est vide

--- website/Controllers/Subscription.php
+++ website/Controllers/Subscription.php
@@ -16,10 +16,21 @@ class Subscription
     public static function getSubscriptionState(\Entities\Request $req): void
     {
         $subscription_id = $req->getGET("subscription_id");
+        if (empty($subscription_id))
+        {
+            http_response_code(400);
+            return;
+        }
         $subscription = (new \Queries\Subscriptions) -> retrieve($subscription_id);
+        if (empty($subscription))
+        {
+            http_response_code(400);
+            return;
+        }
+
 
         // Publish view
-        $data = ["subscription" => $subscription];
+        $data["subscription"] = $subscription;
         \Helpers\DisplayManager::display("subscription", $data);
     }
 }
