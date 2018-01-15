4005a062e58882da88f49612373207255b1d594e
Randy <randy.arnold@hotmail.fr>
2018-01-15 09:28:28 +0100

    lien entre les pages

--- website/Views/Properties/nouvellepropriete/nouvellepropriete.php
+++ website/Views/Properties/nouvellepropriete/nouvellepropriete.php
@@ -1,9 +1,10 @@
-<head>
-    <link rel="stylesheet" href="nouvellepropriete.css" />
-</head>
-
+<main>
+    <br>
+    <br>
+    <br>
 <form id="form" action="index.php?c=Property&a=New" method="POST">
     <label>Nom : </label><input type="text" name="name"/>
     <label>Adresse : </label><input type="text" name="address"/>
     <input type="submit" value="Valider"/>
 </form>
+</main>
