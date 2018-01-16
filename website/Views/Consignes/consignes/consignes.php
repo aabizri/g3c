3f173e141eb3887d05cb185ab49cd0a397236fe0
Bryanau <Bryan.nlg.93@gmail.com>
2018-01-16 23:22:31 +0100

    Réactualise le menu avec les consignes à la place des filtres

--- website/Views/Consignes/consignes/consignes.php
+++ website/Views/Consignes/consignes/consignes.php
@@ -4,7 +4,7 @@
         <li id="Moncompte"><a href="index.php?c=User&a=Informations"><input type="button" value="Mon compte" /></a></li>
         <li id="Mespieces"><a href="index.php?c=Room&a=RoomsPage"><input type="button" value="Mes pièces" /></a></li>
         <li id="Mesperipheriques"> <a href="index.php?c=Peripheral&a=PeripheralsPage"><input type="button" value="Mes périphériques" /></a></li>
-        <li id="Mesfiltres"><a href="index.php?c=Filtre&a=FiltersPage"><input type="button" value="Mes filtres" /></a></li>
+        <li id="Mesconsignes"><a href="index.php?c=Consigne&a=ConsignesPage"><input type="button" value="Mes Consignes" /></a></li>
         <li id="Mesparametres"><a href="Mesparametres.html"><input type="button" value="Mes paramètres" /></a></li>
     </ul>
 
