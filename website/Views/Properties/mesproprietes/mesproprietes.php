e055783d250dd1761fb6229dc89cb84c58abbc53
Randy <randy.arnold@hotmail.fr>
2018-01-16 10:30:18 +0100

    Ajout bouton

--- website/Views/Properties/mesproprietes/mesproprietes.php
+++ website/Views/Properties/mesproprietes/mesproprietes.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html>
 
-<header>
-    <div id="Logo"> <a href="paged'accueil.html"><img src=logopetit.png alt="logopetit.png"> </a></div>
-    <p id="Nom">LiveWell</p>
-    <p id="accroche"><i>Votre sécurité est notre priorité.</i></p>
-    <p id="Nomdelapage">Mes pièces</p>
-</header>
-
-<head>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="mesproprietes.css" />
-    <title>Mes périphériques</title>
-</head>
-
-<body>
     <ul>
     <?php
-    //foreach ($data["properties"] as $property) {
-        //echo "<li><button type="button" id=$property><img scr="" height="100px" width="100px" ".$property."/></button></li>";
+    $properties = $data["properties"];
+    foreach ($properties as $property) {
+        echo "<li><button type='button'><img scr='' height='100px' width='100px'.$property./></button></li>";
+    }
         ?>
 
         <li id="ajouter"><button type="button" id="ajout" onclick='window.location.href = "index.php?c=Property&a=NewProperty"' >
@@ -31,10 +17,3 @@
                 </button><br>Ajouter une propriété</li>
 
     </ul>
-
-
-</body>
-
-
-</html>
-
