5437484476b82b67dee530a0b7a6c2abeec1872b
Randy <randy.arnold@hotmail.fr>
2018-01-18 12:00:09 +0100

    debug = true pour les tests

--- website/Views/Properties/nouvellepropriete/nouvellepropriete.php
+++ website/Views/Properties/nouvellepropriete/nouvellepropriete.php
@@ -2,7 +2,7 @@
     <br>
     <br>
     <br>
-<form id="form" action="index.php?c=Property&a=New" method="POST">
+<form id="form" action="index.php?c=Property&a=New&debug=true" method="POST">
     <label>Nom : </label><input type="text" name="name"/>
     <label>Adresse : </label><input type="text" name="address"/>
     <input type="submit" value="Valider"/>
