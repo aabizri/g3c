a0ee3464e93f53025df1053366d10dec6a34a40b
JeremyBerda <jeremy.berda@icloud.com>
2018-01-18 10:25:24 +0100

    Modifie la vue

--- website/Views/FAQ/faq/faq.php
+++ website/Views/FAQ/faq/faq.php
@@ -1,12 +1,32 @@
 <main>
     <div id="faq">
         <h2 id="titre">Foire Aux Questions</h2>
-        <p>
-            <?php
-            $faq = $data["faq"];
-            echo $faq->getQuestion();
-            ?>
-        </p>
-    </div>
 
-</main>
+        <?php
+        //créer un tableau qui s'indente en fonction du nombre de questions
+        $Nbrdonnees = count($data["questions_list"]);
+        if ($Nbrdonnees != 0) {
+            //on fait le tableau d'un coté question et de l'autre sa réponse
+
+            echo '<thread><tr>
+                    <th> Questions</th>
+                    <th> Réponses</th>
+                    </tr></thread>';
+                    
+            foreach ($data["questions_list"] as $p) {
+
+                //on récupère la question, puis sa réponse en vérifiant qu'il y en a bien une
+                $question = $p->getQuestion();
+
+                if ($p->getAnswer() !== null){
+
+                    $answer = $p->getAnswer();
+                }
+
+                echo '<tr>
+                        <td>'. $question . '</td>
+                        <td>' . $answer . '</td>
+                      </tr>';
+
+            }
+        }
