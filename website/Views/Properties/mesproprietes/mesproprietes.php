e88bf6b9a9f5a82811f4f1fe3760dc4ea60c24b5
Randy <randy.arnold@hotmail.fr>
2018-01-18 11:59:51 +0100

    Vue fonctionnelle

--- website/Views/Properties/mesproprietes/mesproprietes.php
+++ website/Views/Properties/mesproprietes/mesproprietes.php
@@ -1,19 +1,20 @@
 
-    <ul>
-    <?php
-    $properties = $data["properties"];
-    foreach ($properties as $property) {
-        echo "<li><button type='button'><img scr='' height='100px' width='100px'.$property./></button></li>";
-    }
-        ?>
+<ul>
+<?php
+$properties = $data["properties"];
+$lien = 'window.location.href = "index.php?c=Property&a=Dashboard&debug=true"';
+foreach ($properties as $property) {
+    echo "<li>"."<button type='button' id='ajout' onclick='$lien'>"."<img src='https://lh4.ggpht.com/xBh0lfVel9SHv7z8xGKONTXEubFP71NoBZTeC0sWRYz5ZF5L-tABYuzZ9FP2j_lTdw=w300' height='100px' width='100px'>"."</button>"."<br>".$property->getName()."</li>";
+}
+    ?>
 
-        <li id="ajouter"><button type="button" id="ajout" onclick='window.location.href = "index.php?c=Property&a=NewProperty"' >
-                <img
-                        src="Views/Properties/mesproprietes/plus.png"
-                        alt="Ajouter une proptiété"
-                        height="100px"
-                        width="100px"
-                >
-                </button><br>Ajouter une propriété</li>
+    <li id="ajouter"><button type="button" id="ajout" onclick='window.location.href = "index.php?c=Property&a=NewProperty&debug=true"' >
+            <img
+                    src="Views/Properties/mesproprietes/plus.png"
+                    alt="Ajouter une proptiété"
+                    height="100px"
+                    width="100px"
+            >
+            </button><br>Ajouter une propriété</li>
 
-    </ul>
+</ul>
