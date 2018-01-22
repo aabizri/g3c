9c3a5bdf0bef32b94d667ec4111d47203b8ee158
Alexandre A. Bizri <alexandre@bizri.fr>
2018-01-22 16:56:24 +0100

    Ajoute les modules "périphériques" et "produits" (boutique) à la page d'acceuil de la console admin.

--- website/Views/Admin/console/console.php
+++ website/Views/Admin/console/console.php
@@ -4,15 +4,29 @@
 
     <div id="modules">
         <!-- Les modules vont ici -->
-        <div class="module" id="users" onclick="window.location.href = 'index.php?c=Admin&a=Users'">
+        <div class="module" id="users" onclick="window.location.href = 'index.php?c=Admin&a=Users'"
+             title="Visualiser, modifier et supprimer des utilisateurs">
             <img class="module_image" src="Views/Admin/console/images/users.svg"/>
             <p class="module_title">Utilisateurs</p>
             <p class="module_description">Visualiser, modifier et supprimer des utilisateurs</p>
         </div>
-        <div class="module" id="properties" onclick="window.location.href = 'index.php?c=Admin&a=Properties'">
+        <div class="module" id="properties" onclick="window.location.href = 'index.php?c=Admin&a=Properties'"
+             title="Visualiser, modifier et supprimer des propriétés">
             <img class="module_image" src="Views/Admin/console/images/properties.svg"/>
             <p class="module_title">Propriétés</p>
             <p class="module_description">Visualiser, modifier et supprimer des propriétés</p>
+        </div>
+        <div class="module" id="peripherals" onclick="window.location.href = 'index.php?c=Admin&a=Peripherals'"
+             title="Visualiser, modifier et supprimer des périphériques">
+            <img class="module_image" src="Views/Admin/console/images/peripherals.svg"/>
+            <p class="module_title">Périphériques</p>
+            <p class="module_description">Visualiser, modifier et supprimer des périphériques</p>
+        </div>
+        <div class="module" id="products" onclick="window.location.href = 'index.php?c=Admin&a=Products'"
+             title="Visualiser, modifier et supprimer des produits de la boutique">
+            <img class="module_image" src="Views/Admin/console/images/products.svg"/>
+            <p class="module_title">Produits</p>
+            <p class="module_description">Visualiser, modifier et supprimer des produits</p>
         </div>
     </div>
 </main>
