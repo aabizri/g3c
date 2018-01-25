42953d34ded07c59a88284294bec829d802733a1
Randy <randy.arnold@hotmail.fr>
2018-01-25 16:08:17 +0100

    Modification du texte affiché, redirection vers la page de connection

--- website/Views/Users/majmdpreussie/majmdpreussie.php
+++ website/Views/Users/majmdpreussie/majmdpreussie.php
@@ -1,6 +1,7 @@
 <main>
+
     <br>
     <br>
-    <h2>Mise à jour du mot de passe réussie</h2>
-    <a href="index.php?c=User&a=Informations">Revenir à la page <i>mon compte</i></a>
+    <p>Mise à jour du mot de passe réussie, nous vous avons envoyé un mot de passe provisoire par mail</p>
+    <a href="index.php?c=User&a=ConnectionPage">Revenir à la page <i>mon compte</i></a>
 </main>
