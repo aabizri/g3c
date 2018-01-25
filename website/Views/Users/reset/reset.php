12074e589a8f4dc4fc1f0f47b3e50c06c9cb85e8
Randy <randy.arnold@hotmail.fr>
2018-01-25 14:59:25 +0100

    Page de reinitialisation MDP

---
+++ website/Views/Users/reset/reset.php
@@ -0,0 +1,6 @@
+<main>
+    <form id="reinit" method="post" action=action="index.php?c=User&a=Truc">
+        <label>Pseudo ou courriel : </label><input type="text" name="login"/>
+        <input type="submit" value="valider"/>
+    </form>
+</main>
