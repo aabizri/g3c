929b974ea35457f3bbf8190b629064b36769c63e
Alexandre A. Bizri <alexandre@bizri.fr>
2018-01-26 00:05:51 +0100

    Répare un lien non-modifié alors que le nom du contrôleur le fut

--- website/Views/CGU/modificationcgu/modificationcgu.php
+++ website/Views/CGU/modificationcgu/modificationcgu.php
@@ -1,7 +1,7 @@
 <main>
     <div id="modifieCGU">
         <h2>Modification des Conditions générales d'utilisation du site</h2>
-        <form method="post" action="index.php?c=CGU&a=ModificateCGU&debug=true">
+        <form method="post" action="index.php?c=CGU&a=ModifyCGU&debug=true">
             <textarea rows="27" cols="150" wrap="physical" id="textcgu" name="cgu"  >
                 <?php
                 $cgu = $data["cgu"];
