d9193180601e841aa83637d7bcb3bdd324d32533
Alexandre A. Bizri <alexandre@bizri.fr>
2018-01-27 17:30:24 +0100

    Prépare le merge avec master et la re-écriture d'URL

--- website/Controllers/Property.php
+++ website/Controllers/Property.php
@@ -19,16 +19,16 @@ class Property
     //Afficher les utilisateurs d'une propriété
     public static function getProperty(\Entities\Request $req): void
     {
-
         //On récupère les données
         $property = $req->getProperty();
-        if ($property === null){
-            Error::getInternalError500($req);
+        if ($property === null) {
+            http_response_code(400);
+            echo "Erreur: pas de propriété indiquée";
             return;
         }
 
         //On récupère les infos de la propriété
-        $property_id = $property -> getID();
+        $property_id = $property->getID();
 
         //Grace à l'id de la propriété, on récupère tous les ids des roles avec le même id de propriété
         $property_roles = (new \Queries\Roles)->filterByPropertyID("=", $property_id)->find();
@@ -59,21 +59,21 @@ class Property
     public static function postAddUser(\Entities\Request $req)
     {
 
-        //On recupère la donnée et on vérifie qu'elle existe bien
-        $nickname = $req->getPOST('nickname');
-        if ($nickname === null){
-            DisplayManager::redirectToController("Property", "PropertyPage" );
-            return;
-        }
-
         //On récupère l'id de la propriété
         $property_id = $req->getPropertyID();
 
+        //On recupère la donnée et on vérifie qu'elle existe bien
+        $nickname = $req->getPOST('nickname');
+        if ($nickname === null) {
+            DisplayManager::redirectToPath("properties/" . $property_id);
+            return;
+        }
+
         //On récupère le user_id du nickname
         //Si on ne trouve rien, c'est que le nickname n'existe pas
-        $user = (new \Queries\Users) -> filterByNick("=", $nickname) ->findOne();
-        if ($user === null){
-            DisplayManager::redirectToController("Property", "PropertyPage");
+        $user = (new \Queries\Users)->filterByNick("=", $nickname)->findOne();
+        if ($user === null) {
+            DisplayManager::redirectToPath("properties/" . $property_id);
             return;
         }
 
@@ -81,8 +81,8 @@ class Property
         $user_id = $user->getID();
 
         //On vérifie que le nickname n'est pas deja lié à cette propriété
-        if ((new \Queries\Roles) -> filterByUserID("=", $user_id) ->findOne()){
-            DisplayManager::redirectToController("Property", "PropertyPage");
+        if ((new \Queries\Roles)->filterByUserID("=", $user_id)->findOne()) {
+            DisplayManager::redirectToPath("properties/" . $property_id);
             return;
         }
 
@@ -91,82 +91,42 @@ class Property
         $r->setUserID($user_id);
         $r->setPropertyID($property_id);
 
-
         //On insère le role dans la bdd
         (new \Queries\Roles)->save($r);
 
-        DisplayManager::redirectToController("Property", "PropertyPage");
+        DisplayManager::redirectToPath("properties/" . $property_id);
     }
 
 
     //Supprimer un utilisateur de la propriété
-    public static function postDeleteUserFromProperty(\Entities\Request$req){
-
+    public static function postDeleteUserFromProperty(\Entities\Request $req)
+    {
         //On récupère les données
-        $user_id = $req->getPOST('user_id');
-        $property_id = $req -> getPropertyID();
-
-        //On vérifie qu'il appartient bien à la propriété
-        //TODO il faut l'user id en get
-        /*$role = (new \Queries\Roles)
-                -> filterByColumn("property_id", "=", $property_id, "AND" )
-                -> filterByColumn("user_id", "=", $user_id, "AND")
-                -> findOne();
-        if($role === null){
+        $to_delete_user_id = $req->getPOST('user_id');
+        if ($to_delete_user_id === null) {
+            http_response_code(400);
+            echo "Erreur : pas de user à supprimer indiqué";
             return;
-        }*/
+        }
+        $property_id = $req->getPropertyID();
+        if ($property_id === null) {
+            http_response_code(400);
+            echo "Erreur: pas de propriété indiquée";
+            return;
+        }
 
         //On supprime l'utilisateur de la propriété
-        (new \Queries\Roles)
-            -> filterByColumn("property_id", "=", $property_id, "AND" )
-            -> filterByColumn("user_id", "=", $user_id, "AND")
-            -> delete();
-
-        //On affiche la page avec l'utilisateur supprimé
-        self::getProperty($req);
-    }
-
-    //JAVASCRIPT?
-    //Envoyer les users de la propriete en JSON
-    /*public static function getPropertyUsers(\Entities\Request $req){
-
-        //On récupère l'id de la propriété
-        $property_id = $req->getPropertyID();
-
-        //Grace à l'id de la propriété, on récupère tous les ids des roles avec le même id de propriété
-        $property_users_list = \Repositories\Roles::findAllByPropertyID($property_id);
-        if ($property_users_list===null){
-            echo "il n'y a pas d'utilisateurs";
+        $count = (new \Queries\Roles)
+            ->filterByColumn("property_id", "=", $property_id, "AND")
+            ->filterByColumn("user_id", "=", $to_delete_user_id, "AND")
+            ->delete();
+        if ($count !== 1) {
+            http_response_code(500);
+            echo "Erreur: erreur interne lors de la suppression, compte de suppression: " . $count;
             return;
         }
 
-        //On récupère ensuite les entités des roles grace à leurs ids
-        $users_entities_list=[];
-        foreach ($property_users_list as $role_id){
-            $r=\Repositories\Roles::retrieve($role_id);
-            $users_entities_list[] = $r;
-        }
-
-        //Depuis ces entités on récupère les id des utilisateurs
-        $users_DisplayId_list =[];
-        foreach ($users_entities_list as $user_entity){
-            $user_id = $user_entity->getUserId();
-            $users_DisplayId_list[] = $user_id;
-        }
-
-        //Enfin grace aux ids des utilisateurs, on peut récupérer leurs entités (entre autre pour faire apparaitre le nickname)
-        $users_list=[];
-        foreach ($users_list as $uid){
-            $user = \Repositories\Users::retrieve($uid);
-            $users_list[] = $user;
-        }
-
-        //On rajoute le display name de chaque utilisateurs à la liste qui a deja un id
-        foreach ($users_list as $user_entity){
-            $user_display = $user_entity->getDisplay();
-            $users_DisplayId_list [] = $user_display;
-        }
-
-        return json_encode($users_DisplayId_list);
-        }*/
+        //On affiche la page avec l'utilisateur supprimé
+        DisplayManager::redirectToController("Property", "Property");
+    }
 }
--- website/Views/Users/mapropriete/mapropriete.php
+++ website/Views/Users/mapropriete/mapropriete.php
@@ -1,11 +1,15 @@
 <html>
 <ul id="Menu">
-    <li id="Moncompte"><a href="index.php?c=User&a=AccountPage"><input type="button" value="Mon compte" /></a></li>
-    <li id="Mapropriete"><a href="http://localhost/g3c/website/index.php?c=Property&a=PropertyPage"><input type="button" value="Ma propriété" /></a> </li>
-    <li id="Mespieces"><a href="index.php?c=Room&a=RoomsPage"><input type="button" value="Mes pièces" /></a></li>
-    <li id="Mesperipheriques"> <a href="index.php?c=Peripheral&a=PeripheralsPage"><input type="button" value="Mes périphériques" /></a></li>
-    <li id="Mesfiltres"><a href="Mesfiltres.html"><input type="button" value="Mes filtres" /></a></li>
-    <li id="Mesparametres"><a href="Mesparametres.html"><input type="button" value="Mes paramètres" /></a></li>
+    <li id="Moncompte"><a href="index.php?c=User&a=AccountPage"><input type="button" value="Mon compte"/></a></li>
+    <li id="Mapropriete"><a href="http://localhost/g3c/website/index.php?c=Property&a=PropertyPage"><input type="button"
+                                                                                                           value="Ma propriété"/></a>
+    </li>
+    <li id="Mespieces"><a href="index.php?c=Room&a=RoomsPage"><input type="button" value="Mes pièces"/></a></li>
+    <li id="Mesperipheriques"><a href="index.php?c=Peripheral&a=PeripheralsPage"><input type="button"
+                                                                                        value="Mes périphériques"/></a>
+    </li>
+    <li id="Mesfiltres"><a href="Mesfiltres.html"><input type="button" value="Mes filtres"/></a></li>
+    <li id="Mesparametres"><a href="Mesparametres.html"><input type="button" value="Mes paramètres"/></a></li>
 </ul>
 
 <h2 id="titrepage">Ma propriété</h2>
@@ -14,8 +18,8 @@
     <h4>Informations sur ma propriété</h4>
     <?php
     $property = $data["property"];
-    echo "<p><strong>Nom de la propriété : </strong></p>".$property -> getName();
-    echo "<p><strong>Addresse de la propriété : </strong></p>".$property -> getAddress();
+    echo "<p><strong>Nom de la propriété : </strong></p>" . $property->getName();
+    echo "<p><strong>Addresse de la propriété : </strong></p>" . $property->getAddress();
     ?>
 </div>
 
@@ -24,40 +28,42 @@
     <div id="voirutilisateurs">
         <h4 id="titrelisteutilisateurs">Liste des utilisateurs</h4>
         <table id="propertyusers" align="center">
-                <?php
-                //Créer un tableau qui s'indente en fonction du nombre d'utilisateurs
-                $Nbrdonnees = count($data["users_list"]);
-                if ($Nbrdonnees != 0){
-                    //Ici nous faisons le tableau avec ses titres
-                    echo '<thead><tr>
+            <?php
+            //Créer un tableau qui s'indente en fonction du nombre d'utilisateurs
+            $Nbrdonnees = count($data["users_list"]);
+            if ($Nbrdonnees != 0) {
+                //Ici nous faisons le tableau avec ses titres
+                echo '<thead><tr>
                           <th>Utilisateur</th>
                           <th>Gestion</th>
                           </tr></thead>';
 
-                    //Ici nous ajoutons une ligne avec les infos et la possibilité de supprimer la personne
-                    foreach ($data["users_list"] as $u){
-                        echo '<tr>
-                              <td>'. $u->getNick() .'</td>
+                //Ici nous ajoutons une ligne avec les infos et la possibilité de supprimer la personne
+                foreach ($data["users_list"] as $u) {
+                    echo '<tr>
+                              <td>' . $u->getNick() . '</td>
                               <td>
-                                  <form action="index.php?c=Property&a=DeleteUserFromProperty" method="post" >
-                                      <input type="hidden"  name="user_id" value="'. $u->getID() .'" />    
+                                  <form action="index.php?c=Property&a=DeleteUserFromProperty&pid=' . $data["property"]->getID() . '" method="post" >
+                                      <input type="hidden"  name="user_id" value="' . $u->getID() . '" />    
                                       <input type="submit" value="Supprimer"/>
                                   </form>
                               </td>
-                              </tr>';}
-                            }
-                            ?>
+                              </tr>';
+                }
+            }
+            ?>
         </table>
     </div>
 
 
     <div id="creerutilisateur">
         <h4 id="titrecreerutilisateur">Ajouter un utilisateur à la propriété</h4>
-        <form method="post" action="index.php?c=Property&a=NewPropertyUser">
+        <form method="post" action="index.php?c=Property&a=AddUser&debug=true&&pid=<?= $data["property"]->getID() ?>">
             <p>
                 <label>Login</label>: <input id="formdroit" type="text" name="nickname"/>
             </p>
-            <input id="validercreationutilisateur" type="submit" value="Valider" title="Vous créez un nouvel utilisateur"/>
+            <input id="validercreationutilisateur" type="submit" value="Valider"
+                   title="Vous créez un nouvel utilisateur"/>
         </form>
     </div>
 </div>
