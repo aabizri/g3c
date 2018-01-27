415427f424ff092d97ca560b8cefc5b736b22a92
Alexandre A. Bizri <alexandre@bizri.fr>
2018-01-27 18:16:05 +0100

    Répare et prépare à la fusion de master (re-écriture d'URL à venir)

--- website/Controllers/Property.php
+++ website/Controllers/Property.php
@@ -12,93 +12,33 @@ use Entities;
  */
 class Property
 {
-    /**
-     * Create a property
-     * @param Entities\Request $req
-     */
-
-    public static function postJoin(\Entities\Request $req): void
+    public static function getSelect(\Entities\Request $req): void
     {
-        // Récupere le post
-        $post = $req->getAllPOST();
-
-        // Check if the data exists
-        $required = ["address", "name"];
-        foreach ($required as $key) {
-            if (empty($post[$key])) {
-                echo "Missing key: " . $key;
-                return;
-            }
+        $u = $req->getUser();
+        if ($u === null) {
+            http_response_code(403);
+            return;
         }
 
-        // Create the entit
-        // Include la page de confirmation
-        \Helpers\DisplayManager::redirectToController("Property", "Dashboard");
-    }
-
-
-    public function postCreate(\Entities\Request $req): void
-    {
-        // Check if the data exists
-        $required = ["name", "address"];
-        foreach ($required as $key) {
-            if (empty($req->getPOST($key))) {
-                echo "Missing key: " . $key;
-                return;
-            }
-        }
-
-        // Assign values
-        $name = $req->getPOST("name");
-        $address = $req->getPOST("address");
-
-        // Create the entity
-        $p = new Entities\Property();
-        $p->setName($name);
-        $p->setAddress($address);
-
-        // Insert it
-        try {
-            (new \Queries\Properties)
-                ->save($p);
-        } catch (\Exception $e) {
-            echo "Error inserting property" . $e;
-        }
-    }
-
-
-    public static function getMyProperties(\Entities\Request $req): void
-    {
-        $user_id = $req -> getUserID();
-        $user_id = 4;
-
-        // Récupère l'ID de l'utilisatreur
-        //$u = $req->getUser();
-        //if ($u === null) {
-            //http_response_code(403);
-            //return;
-        //}
         // Récupère tous les rôles associés à l'utilisateur
-
         // Pour chaque rôle, tu récupère la propriété associé, et tu l'ajoute à une liste
-        //$data["properties"] = $properties;
-
-        $roles = (new \Queries\Roles) -> filterByUserID("=", $user_id) -> find();
+        $roles = (new \Queries\Roles)->filterByUser("=", $u)->find();
         $properties_id = [];
         foreach ($roles as $r){
             $properties_id[] = $r -> getPropertyID();
         }
 
-        $properties = [];
+        $properties_query = new \Queries\Properties;
         foreach ($properties_id as $pid){
-            $properties[] = (new \Queries\Properties) -> retrieve($pid);
+            $properties[] = $properties_query->filterByColumn("id", "=", $pid, "OR");
         }
+        $properties = $properties_query->find();
         $data["properties"] = $properties;
 
         \Helpers\DisplayManager::display("mesproprietes", $data);
     }
 
-    public static function getNewProperty(\Entities\Request $req): void
+    public static function getNew(\Entities\Request $req): void
     {
         \Helpers\DisplayManager::display("nouvellepropriete");
     }
@@ -143,10 +83,6 @@ class Property
         }
 
         // Include la page de confirmation
-        \Helpers\DisplayManager::redirectToController("Property", "MyProperties");
+        \Helpers\DisplayManager::redirectToPath("index.php?c=Property&a=Select");
     }
-
-
-
-
 }
--- website/Views/Properties/mesproprietes/mesproprietes.php
+++ website/Views/Properties/mesproprietes/mesproprietes.php
@@ -1,20 +1,30 @@
-
+<main>
 <ul>
-<?php
-$properties = $data["properties"];
-$lien = 'window.location.href = "index.php?c=Property&a=Dashboard&debug=true"';
-foreach ($properties as $property) {
-    echo "<li>"."<button type='button' id='ajout' onclick='$lien'>"."<img src='https://lh4.ggpht.com/xBh0lfVel9SHv7z8xGKONTXEubFP71NoBZTeC0sWRYz5ZF5L-tABYuzZ9FP2j_lTdw=w300' height='100px' width='100px'>"."</button>"."<br>".$property->getName()."</li>";
-}
-    ?>
+    <?php
+    $properties = $data["properties"];
+    foreach ($properties as $property) : ?>
+        <li>
+            <a href="index.php?c=Property&a=Property&pid=<?= $property->getID() ?>">
+                <button type='button' id='ajout'>
+                    <img src='https://lh4.ggpht.com/xBh0lfVel9SHv7z8xGKONTXEubFP71NoBZTeC0sWRYz5ZF5L-tABYuzZ9FP2j_lTdw=w300'
+                         height='100px' width='100px'>
+                </button>
+                <br><?= $property->getName() ?>
+            </a>
+        </li>
+    <?php endforeach ?>
 
-    <li id="ajouter"><button type="button" id="ajout" onclick='window.location.href = "index.php?c=Property&a=NewProperty&debug=true"' >
-            <img
-                    src="Views/Properties/mesproprietes/plus.png"
-                    alt="Ajouter une proptiété"
-                    height="100px"
-                    width="100px"
-            >
-            </button><br>Ajouter une propriété</li>
+    <li id="ajouter"><a href="index.php?c=Property&a=New">
+            <button type="button" id="ajout">
+                <img
+                        src="Views/Properties/mesproprietes/plus.png"
+                        alt="Ajouter une proptiété"
+                        height="100px"
+                        width="100px"
+                >
+            </button>
+        </a><br>Ajouter une propriété
+    </li>
 
 </ul>
+</main>
