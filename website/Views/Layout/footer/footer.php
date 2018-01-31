971932beba5e390a4b9eccc053d1be7aea9b8541
JeremyBerda <jeremy.berda@icloud.com>
2018-01-31 18:15:31 +0100

    Fais redirection faq et cgu

--- website/Views/Layout/footer/footer.php
+++ website/Views/Layout/footer/footer.php
@@ -1,7 +1,7 @@
     <footer>
         <p id="SAV">Contact :<br>28 Rue Notre Dame des Champs<br/>75006<br/>Paris<br/>
             <a id="faq" href="faq">Foire aux questions</a><br>
-            <a id="cgu" href="Conditions générales d'utilisation">Conditions générales d'utilisation</a></p>
+            <a id="cgu" href="cgu">Conditions générales d'utilisation</a></p>
         <p class="Renseignements" id="Reseauxsociaux">Réseaux sociaux</p>
         <a href="https://www.facebook.com/"><img id="Fb" src="http://tous-logos.com/wp-content/uploads/2017/07/Le-logo-Facebook.png"/></a>
         <a href="https://plus.google.com/"><img id="Google" src="https://png.icons8.com/color/1600/google-plus-squared.png"/></a>
